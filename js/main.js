e3c9ba1fa7794bbd16c49a8eb7d73e02a935677f
perfectfoolish <DZKXperfect@gmail.com>
2016-01-18 13:32:13 +0800

    Add responsive sidebar

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
-$(".app-footer a").each(function(){
+$(".app-footer").each(function(){
+  var path = document.location.pathname.split('/');
+  var page = path[path.length - 1];
+  var href = $(this).attr("href");
+  if (href === page) {
+    $(this).addClass("active");
+    return false;
+  }
+});
+
+$(".sidebar a").each(function(){
   var path = document.location.pathname.split('/');
   var page = path[path.length - 1];
   var href = $(this).attr("href");
